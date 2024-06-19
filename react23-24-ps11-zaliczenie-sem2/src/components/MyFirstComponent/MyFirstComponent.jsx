4909efd05b8298ecfdeb564377b69953815c3f11
marwas <martus90@wp.pl>
2024-06-19 22:00:49 +0200

    skasowanie niepotrzebnego pliku

--- react23-24-ps11-zaliczenie-sem2/src/components/MyFirstComponent/MyFirstComponent.jsx
+++
@@ -1,5 +0,0 @@
-const MyFirstComponent = () => {
-  return <p> This is a text from my first component</p>;
-};
-
-export default MyFirstComponent;
--- react23-24-ps11-zaliczenie-sem2/src/components/zaliczenie/ProductsFilters/ProductsFilters.jsx
+++
@@ -1,7 +0,0 @@
-import "../commonStyles.css";
-
-const ProductsFilters = () => {
-  return <div className="Wrapper">Products Filters</div>;
-};
-
-export default ProductsFilters;
